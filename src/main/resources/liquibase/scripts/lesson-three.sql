a5e91ab5e3e0a2703d24e665f59c5bdc589c68d7
ElenaR <llinusik@mail.ru>
2022-12-20 10:50:26 +0300

    4.3 готово

--- src/main/resources/liquibase/scripts/lesson-three.sql
+++ src/main/resources/liquibase/scripts/lesson-three.sql
@@ -1,9 +1,9 @@
 -- liquibase formatted sql
 
--- changeset belous:1
+-- changeset: belous:1
 
 CREATE INDEX idx_students_name ON student (name);
 
--- changeset belous:2
+-- changeset: belous:2
 
 CREATE UNIQUE INDEX idx_faculties_name_color ON faculty (name, color);
