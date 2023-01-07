a3028674e6e85d697552b259f46c8c40b1b265ab
ElenaR <llinusik@mail.ru>
2023-01-07 20:08:48 +0300

    4.5 готово

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,7 +1,5 @@
 package ru.hogwarts.school.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +13,7 @@ public class InfoController {
     private final InfoService infoService;
 
 
-
-@Value("${server.port}")
+    @Value("${server.port}")
     private int port;
 
     public InfoController(InfoService infoService) {
@@ -24,10 +21,15 @@ public class InfoController {
     }
 
     @GetMapping("/port")
-    public int getPort(){
-    return port;
+    public int getPort() {
+        return port;
     }
 
+    @GetMapping("/testParallelStream")
+    public void testParallelStream() {
 
-
+    }
 }
+
+
+
--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -1,8 +1,10 @@
 package ru.hogwarts.school.service;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
 import java.util.stream.Stream;
 
@@ -10,7 +12,27 @@ import java.util.stream.Stream;
 public class InfoService {
     private static final Logger LOG = LoggerFactory.getLogger(InfoService.class);
 
-    int sum = Stream.iterate(1, a -> a +1)
-            .limit(1_000_000)
-            .reduce(0, (a, b) -> a + b );
+    public void testParallelStream(){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("plain stream");
+        long sum = Stream.iterate(1L, a -> a +1)
+                .limit(1_000_000L)
+                .reduce(0L, (a, b) -> a + b );
+        stopWatch.stop();
+        LOG.info("Calculated value is {}; {}", sum, stopWatch.prettyPrint());
+        stopWatch.start("parallel stream");
+         sum = Stream.iterate(1L, a -> a +1)
+                .limit(1_000_000L)
+                .reduce(0L, (a, b) -> a + b );
+        stopWatch.stop();
+        LOG.info("Calculated value is {}; {}", sum, stopWatch.prettyPrint());
+
+
+    }
+
+
+
+//    int sum = Stream.iterate(1, a -› a +1) .
+//            limit(1_000_000) .parallel()
+//            .reduce(0, (a, b) -› a + b );
 }
